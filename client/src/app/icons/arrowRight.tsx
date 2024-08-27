a0488d97d06066dee2ce3e74cf65ac663e2c4eea
inakicarcereny <inakicarce@hotmail.com.ar>
2024-08-27 18:45:22 -0300

    add icons

---
+++ client/src/app/icons/arrowRight.tsx
@@ -0,0 +1,11 @@
+export function ArrowRight() {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-arrow-narrow-right text-white" width="26" height="26" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M5 12l14 0" />
+      <path d="M15 16l4 -4" />
+      <path d="M15 8l4 4" />
+    </svg>
+  )
+} 
---
+++ client/src/app/icons/dashboard.tsx
@@ -0,0 +1,11 @@
+export function Dashboard() {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-dashboard text-blue-600" width="44" height="44" viewBox="0 0 24 24" stroke="currentColor" fill="none">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 13m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+      <path d="M13.45 11.55l2.05 -2.05" />
+      <path d="M6.4 20a9 9 0 1 1 11.2 0z" />
+    </svg>
+  )
+}
