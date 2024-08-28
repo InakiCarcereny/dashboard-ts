08eec42e0118a3a0eb2968c74acd206b8c07c7b5
inakicarcereny <inakicarce@hotmail.com.ar>
2024-08-28 19:47:01 -0300

    add icons

---
+++ client/src/app/icons/hamburguer.tsx
@@ -0,0 +1,18 @@
+export function Hamburguer() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-menu-2 text-zinc-600"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      fill="none"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 6l16 0" />
+      <path d="M4 12l16 0" />
+      <path d="M4 18l16 0" />
+    </svg>
+  );
+}
---
+++ client/src/app/icons/moon.tsx
@@ -0,0 +1,20 @@
+export function Moon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-moon-filled text-blue-600"
+      width="26"
+      height="26"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      fill="none"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M12 1.992a10 10 0 1 0 9.236 13.838c.341 -.82 -.476 -1.644 -1.298 -1.31a6.5 6.5 0 0 1 -6.864 -10.787l.077 -.08c.551 -.63 .113 -1.653 -.758 -1.653h-.266l-.068 -.006l-.06 -.002z"
+        stroke-width="0"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
