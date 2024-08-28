6a03ad2671ca86a698ada7faadad7a9000281495
inakicarcereny <inakicarce@hotmail.com.ar>
2024-08-28 19:51:09 -0300

    welcome page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,14 +6,14 @@ const buttons = [
   {
     label: "Log in",
     icon: ArrowRight(),
-    href: "/login"
+    href: "/login",
   },
   {
     label: "Register",
     icon: ArrowRight(),
-    href: "/register"
-  }
-]
+    href: "/register",
+  },
+];
 
 export default function Home() {
   return (
@@ -23,19 +23,17 @@ export default function Home() {
         <h1 className="font-semibold text-4xl">Welcome to the dashboard</h1>
       </div>
 
-      <div className="flex items-center justify-center gap-8 mt-10">
-        {
-          buttons.map((button, index) => {
-            return (
-              <Link key={index} href={button.href}>
-                <button className="flex items-center justify-center gap-4 bg-blue-600 px-4 py-2 hover:rounded-xl duration-200 text-white font-semibold">
-                  {button.label}
-                  {button.icon}
-                </button>
-              </Link>
-            )
-          })
-        }
+      <div className="flex items-center justify-center gap-8 mt-16">
+        {buttons.map((button, index) => {
+          return (
+            <Link key={index} href={button.href}>
+              <button className="flex items-center justify-center gap-4 bg-blue-600 px-4 py-2 hover:rounded-xl duration-200 text-white font-semibold">
+                {button.label}
+                {button.icon}
+              </button>
+            </Link>
+          );
+        })}
       </div>
     </section>
   );
