21014c42a2720e6bc650c7ff1ac40db91d36c606
inakicarcereny <inakicarce@hotmail.com.ar>
2024-08-29 21:25:21 -0300

    add cors credentials

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/auth", routes);
