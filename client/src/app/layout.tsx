8ba367a69675b1fd76894c4b5e5b6ec6ccc2339e
inakicarcereny <inakicarce@hotmail.com.ar>
2024-08-29 21:25:57 -0300

    use user provider

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
+import { UserProvider } from "@/app/context/user";
+
 const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <UserProvider>{children}</UserProvider>
+      </body>
     </html>
   );
 }
