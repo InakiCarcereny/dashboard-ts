bab8bab381588077f1d07420fae2d410b30ba0f8
inakicarcereny <inakicarce@hotmail.com.ar>
2024-09-03 13:30:41 -0300

    add icons

---
+++ client/src/app/icons/dot.tsx
@@ -0,0 +1,20 @@
+export function Dot() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-point-filled"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      fill="none"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        d="M12 7a5 5 0 1 1 -4.995 5.217l-.005 -.217l.005 -.217a5 5 0 0 1 4.995 -4.783z"
+        stroke-width="0"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
