1d2cfa6bf911250ecb1aec80de009a7b701d0361
inakicarcereny <inakicarce@hotmail.com.ar>
2024-09-04 10:28:58 -0300

    add icons

---
+++ client/src/app/icons/acces.tsx
@@ -0,0 +1,20 @@
+export function Acces() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-access-point"
+      width="26"
+      height="26"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      fill="none"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 12l0 .01" />
+      <path d="M14.828 9.172a4 4 0 0 1 0 5.656" />
+      <path d="M17.657 6.343a8 8 0 0 1 0 11.314" />
+      <path d="M9.168 14.828a4 4 0 0 1 0 -5.656" />
+      <path d="M6.337 17.657a8 8 0 0 1 0 -11.314" />
+    </svg>
+  );
+}
---
+++ client/src/app/icons/list.tsx
@@ -0,0 +1,21 @@
+export function List() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-list"
+      width="26"
+      height="26"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      fill="none"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 6l11 0" />
+      <path d="M9 12l11 0" />
+      <path d="M9 18l11 0" />
+      <path d="M5 6l0 .01" />
+      <path d="M5 12l0 .01" />
+      <path d="M5 18l0 .01" />
+    </svg>
+  );
+}
