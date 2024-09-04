21663fa6be0d6c286c60c343c01c1a6f2bdd5d65
inakicarcereny <inakicarce@hotmail.com.ar>
2024-09-04 10:30:59 -0300

    compose dashboard page

---
+++ client/src/app/(routes)/dashboard/components/secondSection/SecondSection.dashboard.tsx
@@ -0,0 +1,13 @@
+import { Events } from "../events/Events.dashboard";
+
+export function SecondSection() {
+  return (
+    <div className="flex flex-col gap-4">
+      <Events />
+
+      <div className="h-[275px] w-[400px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
+        .
+      </div>
+    </div>
+  );
+}
--- client/src/app/(routes)/dashboard/page.tsx
+++ client/src/app/(routes)/dashboard/page.tsx
@@ -1,45 +1,16 @@
 "use client";
 
+import { Dot } from "@/app/icons/dot";
+import { Pin } from "@/app/icons/pin";
+import { FirstSection } from "./components/firstSection/FirstSection.dashboard";
+import { SecondSection } from "./components/secondSection/SecondSection.dashboard";
+
 export default function Dashboard() {
   return (
     <div className="flex gap-4">
-      <div className="flex flex-col gap-4">
-        <div className="flex gap-4">
-          <div className="h-[200px] w-[600px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-            .
-          </div>
+      <FirstSection />
 
-          <div className="h-[200px] w-[600px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-            .
-          </div>
-        </div>
-        <div className="h-[385px] w-[1215px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-          .
-        </div>
-        <div className="flex gap-4">
-          <div className="h-[250px] w-[292px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-            .
-          </div>
-          <div className="h-[250px] w-[292px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-            .
-          </div>
-          <div className="h-[250px] w-[292px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-            .
-          </div>
-          <div className="h-[250px] w-[292px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-            .
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-4">
-        <div className="h-[600px] w-[400px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-          .
-        </div>
-        <div className="h-[250px] w-[400px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-          .
-        </div>
-      </div>
+      <SecondSection />
     </div>
   );
 }
