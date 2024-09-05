7e5f6675efcaf6e9016cb5200dbcb4463bdda063
inakicarcereny <inakicarce@hotmail.com.ar>
2024-09-05 16:38:02 -0300

    add calendar component to SecondSection with date state management for better event scheduling in the dashboard

--- client/src/app/(routes)/dashboard/components/secondSection/SecondSection.dashboard.tsx
+++ client/src/app/(routes)/dashboard/components/secondSection/SecondSection.dashboard.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import { Events } from "../events/Events.dashboard";
+import { Calendar } from "@/components/ui/calendar";
 
 export function SecondSection() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
   return (
     <div className="flex flex-col gap-4">
       <Events />
 
-      <div className="h-[275px] w-[365px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40">
-        .
+      <div className="h-[275px] w-[375px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl flex items-center justify-center">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="dark:text-white  text-black"
+        />
       </div>
     </div>
   );
