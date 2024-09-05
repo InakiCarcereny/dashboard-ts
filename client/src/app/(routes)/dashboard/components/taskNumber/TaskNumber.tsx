0731f093a13984e909f62265b78be63c41ba7f2f
inakicarcereny <inakicarce@hotmail.com.ar>
2024-09-05 16:39:20 -0300

    add TaskNumber component to display the number of tasks with responsive design and improved styling in the dashboard

---
+++ client/src/app/(routes)/dashboard/components/taskNumber/TaskNumber.tsx
@@ -0,0 +1,16 @@
+import { useTask } from "@/app/context/task";
+import { File } from "@/app/icons/file";
+
+export function TaskNumber() {
+  const { tasks } = useTask();
+
+  return (
+    <div className="h-[200px] w-[600px] bg-zinc-300 dark:bg-zinc-700/40 rounded-xl dark:text-zinc-700/40 px-4 py-2">
+      <span className="flex items-center gap-2 font-semibold dark:text-white text-xl">
+        <File />
+        Number of tasks
+      </span>
+      <p className="font-semibol dark:text-white text-4xl">{tasks.length}</p>
+    </div>
+  );
+}
