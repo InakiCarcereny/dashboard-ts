abbf647fb68db019b9703fc08da31096d7598258
inakicarcereny <inakicarce@hotmail.com.ar>
2024-09-11 21:56:36 -0300

    Integrate react-big-calendar with dayjs in Calendar component; improve layout and remove unnecessary elements for better display

--- client/src/app/(routes)/dashboard/(routes)/calendar/page.tsx
+++ client/src/app/(routes)/dashboard/(routes)/calendar/page.tsx
@@ -1,7 +1,20 @@
-export default function Calendar() {
+"use client";
+
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import dayjs from "dayjs";
+
+export default function Page() {
+  const localizer = dayjsLocalizer(dayjs);
+
   return (
-    <div>
-      <h1>Calendar</h1>
+    <div className="h-[98%] w-full">
+      <Calendar
+        localizer={localizer}
+        events={[]}
+        startAccessor="start"
+        endAccessor="end"
+      />
     </div>
   );
 }
