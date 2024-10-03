55e0c16270595b1776c68892e58af3f0823e6031
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-03 13:31:39 -0300

    fix updateTask controller to log errors in response for better debugging and error handling in task updates

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -84,9 +84,7 @@ export const updateTask = async (req, res) => {
     const { title, description, dueDate } = req.body;
 
     const updateTask = await Task.findByIdAndUpdate(
-      {
-        id,
-      },
+      id,
       {
         title,
         description,
@@ -98,7 +96,8 @@ export const updateTask = async (req, res) => {
     );
     res.status(200).json(updateTask);
   } catch (err) {
-    res.status(500).json({ message: "Error updating task" });
+    console.log(err);
+    res.status(500).json({ message: "Error updating task", err });
   }
 };
 
