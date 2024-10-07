a02b93a0cd88906e982ecc023106d34431d7ea61
inakicarcereny <inakicarce@hotmail.com.ar>
2024-10-07 16:31:11 -0300

    refactor Company component: replace list with table layout for better company display, integrate delete functionality, and add message handling

--- client/src/app/(routes)/dashboard/(routes)/companies/components/company/Company.tsx
+++ client/src/app/(routes)/dashboard/(routes)/companies/components/company/Company.tsx
@@ -1,23 +1,118 @@
-import { type Company as CompanyType } from "@/app/(routes)/dashboard/(routes)/companies/context/company";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useCompany } from "../../context/company";
+import { toast } from "sonner";
+import { Trash } from "@/app/icons/trash";
+import { useEffect, useState } from "react";
 
-export function Company({
-  name,
-  logo,
-  revenue,
-  size,
-  type,
-  country,
-}: CompanyType) {
+export function CompanyTable({ query }: { query: string }) {
+  const [message, setMessage] = useState("");
+
+  const { companies, deleteCompany, error } = useCompany();
+
+  const filteredCompanies = companies.filter((company) =>
+    company.name.toLocaleLowerCase().includes(query)
+  );
+
+  useEffect(() => {
+    if (companies.length === 8) {
+      setMessage("You can only have 8 companies at the moment");
+      return;
+    } else {
+      setMessage("");
+    }
+  }, [companies]);
+
   return (
-    <li className="flex items-center justify-between mt-6">
-      <div className="flex items-center gap-2">
-        <img className="w-14 h-14" src={logo} alt="company logo" />
-        <span>{name}</span>
-      </div>
-      <span>{revenue}</span>
-      <span>{size}</span>
-      <span>{type}</span>
-      <span>{country}</span>
-    </li>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow className="w-full border-none">
+            <TableHead className="w-[400px] font-semibold text-blue-600 text-base sm:text-xl">
+              Name
+            </TableHead>
+            <TableHead className="w-[400px] font-semibold text-blue-600 text-base sm:text-xl">
+              Revenue
+            </TableHead>
+            <TableHead className="w-[400px] font-semibold text-blue-600 text-base sm:text-xl">
+              Size
+            </TableHead>
+            <TableHead className="w-[400px] font-semibold text-blue-600 text-base sm:text-xl">
+              Type
+            </TableHead>
+            <TableHead className="w-[400px] font-semibold text-blue-600 text-base sm:text-xl">
+              Country
+            </TableHead>
+            <TableHead className="w-[400px] font-semibold text-blue-600 text-base sm:text-xl">
+              Actions
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filteredCompanies.map((company) => {
+            if (
+              company.logo &&
+              company.logo.data &&
+              Array.isArray(company.logo.data.data)
+            ) {
+              const logoUrl = `data:${
+                company.logo.contentType
+              };base64,${Buffer.from(company.logo.data.data).toString(
+                "base64"
+              )}`;
+
+              return (
+                <TableRow
+                  key={company._id}
+                  className="font-semibold hover:bg-zinc-300 dark:hover:bg-zinc-700/40 border-none"
+                >
+                  <TableCell>
+                    <div className="flex items-center gap-4">
+                      <img
+                        className="w-14 h-14"
+                        src={logoUrl}
+                        alt={company.name}
+                      />
+                      <span className="dark:text-white font-semibold">
+                        {company.name}
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell>{company.revenue}</TableCell>
+                  <TableCell>{company.size}</TableCell>
+                  <TableCell>{company.type}</TableCell>
+                  <TableCell>{company.country}</TableCell>
+                  <TableCell className="flex flex-col sm:flex-row items-center gap-2">
+                    <button
+                      onClick={() => {
+                        deleteCompany(company);
+                        toast.success("Company deleted successfully");
+                      }}
+                      className="px-2 py-2 rounded-full cursor-pointer"
+                    >
+                      <Trash />
+                    </button>
+                  </TableCell>
+                </TableRow>
+              );
+            }
+          })}
+        </TableBody>
+      </Table>
+      <p className="text-red-500 text-sm font-semibold">{message}</p>
+      {error.map((err) => {
+        return (
+          <span key={err} className="text-red-500 text-sm font-semibold">
+            {err}
+          </span>
+        );
+      })}
+    </>
   );
 }
--- client/src/app/(routes)/dashboard/(routes)/companies/page.tsx
+++ client/src/app/(routes)/dashboard/(routes)/companies/page.tsx
@@ -1,54 +1,24 @@
 "use client";
 
 import { useState } from "react";
-import { useCompany } from "@/app/(routes)/dashboard/(routes)/companies/context/company";
-import { type Company as CompanyType } from "@/app/(routes)/dashboard/(routes)/companies/context/company";
 import { FormModal } from "./components/form/FormModal.companies";
-import { Company } from "./components/company/Company";
 import { Header } from "./components/header/Header.companies";
+import { CompanyTable } from "./components/company/Company";
 
-export default function Companies({ query }: { query: string }) {
+export default function Companies({ searchParams }: { searchParams: any }) {
   const [open, setOpen] = useState(false);
 
-  const { companies } = useCompany();
+  const query = searchParams?.query || "";
 
   const handleOpen = () => {
     setOpen(!open);
   };
 
-  const filteredCompanies = companies.filter((company) =>
-    company.name.toLocaleLowerCase().includes(query)
-  );
-
   return (
     <section className="flex flex-col gap-4 w-full h-full">
       <Header open={open} handleOpen={handleOpen} />
 
-      <ul>
-        {companies.map((company: CompanyType) => {
-          if (
-            company.logo &&
-            company.logo.data &&
-            Array.isArray(company.logo.data.data)
-          ) {
-            const logoUrl = `data:${
-              company.logo.contentType
-            };base64,${Buffer.from(company.logo.data.data).toString("base64")}`;
-
-            return (
-              <Company
-                key={company._id}
-                name={company.name}
-                logo={logoUrl}
-                revenue={company.revenue}
-                size={company.size}
-                type={company.type}
-                country={company.country}
-              />
-            );
-          }
-        })}
-      </ul>
+      <CompanyTable query={query} />
 
       {open && <FormModal setOpen={setOpen} />}
     </section>
